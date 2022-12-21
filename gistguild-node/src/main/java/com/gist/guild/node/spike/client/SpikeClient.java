7ff0b0731f849d7567d5c3c4a31a4272d692ee2f
Giuseppe Vincenzi <gdvincenzi@airfrance.fr>
2022-12-21 02:20:30 +0100

    Insert new product

--- gistguild-node/src/main/java/com/gist/guild/node/spike/client/SpikeClient.java
+++ gistguild-node/src/main/java/com/gist/guild/node/spike/client/SpikeClient.java
@@ -1,11 +1,18 @@
 package com.gist.guild.node.spike.client;
 
 import com.gist.guild.commons.message.DistributionMessage;
+import com.gist.guild.commons.message.entity.DocumentProposition;
+import com.gist.guild.node.spike.configuration.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "spike", url = "${gistguild.spike.url}")
+@FeignClient(name = "spike", url = "${gistguild.spike.url}", configuration = FeignClientConfiguration.class)
 public interface SpikeClient {
     @PostMapping("/verify")
     DistributionMessage<Void> integrityVerification();
+
+    @PostMapping("/document")
+    ResponseEntity<DistributionMessage<DocumentProposition>> itemProposition(@RequestBody DocumentProposition proposition);
 }
--- gistguild-node/src/main/java/com/gist/guild/node/spike/controller/NodeProductViewController.java
+++ gistguild-node/src/main/java/com/gist/guild/node/spike/controller/NodeProductViewController.java
@@ -1,6 +1,10 @@
 package com.gist.guild.node.spike.controller;
 
 import com.gist.guild.commons.exception.GistGuildGenericException;
+import com.gist.guild.commons.message.DistributionMessage;
+import com.gist.guild.commons.message.DocumentPropositionType;
+import com.gist.guild.commons.message.entity.DocumentProposition;
+import com.gist.guild.commons.message.entity.Participant;
 import com.gist.guild.node.core.configuration.StartupConfig;
 import com.gist.guild.node.core.document.Product;
 import com.gist.guild.node.core.repository.ProductRepository;
@@ -9,6 +13,7 @@ import com.gist.guild.node.spike.client.SpikeClient;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,6 +44,33 @@ public class NodeProductViewController {
         model.addAttribute("validation", nodeService.validate(items));
         model.addAttribute("startup", StartupConfig.getStartupProcessed());
         model.addAttribute("items", items);
+
+        model.addAttribute("newProduct", new com.gist.guild.commons.message.entity.Product());
+
+        return "product"; //view
+    }
+
+    @PostMapping("/product")
+    public String newProduct(@ModelAttribute com.gist.guild.commons.message.entity.Product newProduct, Model model) throws GistGuildGenericException, InterruptedException {
+        long countBefore = repository.count();
+
+        DocumentProposition documentProposition = new DocumentProposition();
+        documentProposition.setDocumentPropositionType(DocumentPropositionType.PRODUCT_REGISTRATION);
+        documentProposition.setDocumentClass(com.gist.guild.commons.message.entity.Product.class.getSimpleName());
+        documentProposition.setDocument(newProduct);
+        spikeClient.itemProposition(documentProposition);
+
+        while(repository.count() == countBefore){
+            Thread.sleep(1000);
+        }
+
+        List<Product> items = repository.findAll();
+        model.addAttribute("instanceName", instanceName);
+        model.addAttribute("validation", nodeService.validate(items));
+        model.addAttribute("startup", StartupConfig.getStartupProcessed());
+        model.addAttribute("items", items);
+
+        model.addAttribute("newProduct", new com.gist.guild.commons.message.entity.Product());
 
         return "product"; //view
     }
