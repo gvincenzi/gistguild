ca70c3ab4414d1b123ffb00205b6ce9b5486d825
Giuseppe Vincenzi <gdvincenzi@airfrance.fr>
2022-12-21 22:09:57 +0100

    Modify product

--- gistguild-node/src/main/java/com/gist/guild/node/spike/controller/NodeProductViewController.java
+++ gistguild-node/src/main/java/com/gist/guild/node/spike/controller/NodeProductViewController.java
@@ -1,10 +1,8 @@
 package com.gist.guild.node.spike.controller;
 
 import com.gist.guild.commons.exception.GistGuildGenericException;
-import com.gist.guild.commons.message.DistributionMessage;
 import com.gist.guild.commons.message.DocumentPropositionType;
 import com.gist.guild.commons.message.entity.DocumentProposition;
-import com.gist.guild.commons.message.entity.Participant;
 import com.gist.guild.node.core.configuration.StartupConfig;
 import com.gist.guild.node.core.document.Product;
 import com.gist.guild.node.core.repository.ProductRepository;
@@ -13,13 +11,14 @@ import com.gist.guild.node.spike.client.SpikeClient;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Iterator;
 import java.util.List;
 
 @Log
@@ -50,6 +49,28 @@ public class NodeProductViewController {
         return "product"; //view
     }
 
+    @GetMapping("/product/{id}")
+    public String prepareModifyProduct(Model model, @PathVariable String id) throws GistGuildGenericException {
+        List<Product> items = repository.findAll();
+        com.gist.guild.commons.message.entity.Product toModify = new com.gist.guild.commons.message.entity.Product();
+        model.addAttribute("instanceName", instanceName);
+        Iterator<Product> productIterator = items.iterator();
+
+        while(productIterator.hasNext()){
+            Product next = productIterator.next();
+            if(next.getId().equals(id)){
+                toModify = next;
+                break;
+            }
+        }
+
+        model.addAttribute("validation", nodeService.validate(items));
+        model.addAttribute("startup", StartupConfig.getStartupProcessed());
+        model.addAttribute("items", items);
+        model.addAttribute("newProduct", toModify);
+        return "product"; //view
+    }
+
     @PostMapping("/product")
     public String newProduct(@ModelAttribute com.gist.guild.commons.message.entity.Product newProduct, Model model) throws GistGuildGenericException, InterruptedException {
         long countBefore = repository.count();
@@ -60,9 +81,7 @@ public class NodeProductViewController {
         documentProposition.setDocument(newProduct);
         spikeClient.itemProposition(documentProposition);
 
-        while(repository.count() == countBefore){
-            Thread.sleep(1000);
-        }
+        Thread.sleep(5000);
 
         List<Product> items = repository.findAll();
         model.addAttribute("instanceName", instanceName);
